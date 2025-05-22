509809fd860725ec322880a95b480ba26550ac74
GojoSatoru-776 <levdobriy2012@mail.ru>
2025-05-22 21:27:23 +0500

    Add files via upload

--- generate_password52/password.py
+++ generate_password52/password.py
@@ -1,6 +1,6 @@
 import random as rm
 
-def p_a_s_s(num):
+def pass_hard(num):
     str1 = '1234567890'
     str2 = 'qwertyuiopasdfghjklzxcvbnm'
     str3 = str2.upper()
@@ -9,3 +9,10 @@ def p_a_s_s(num):
     rm.shuffle(ls)
     psw = ''.join([rm.choice(ls) for x in range(num)])
     print(f'Password: {psw}')
+
+def pass_lite(eat_num, num_num):
+    eat = ['Apple', 'Orange', 'Pineapple', 'Watermelon', 'Tomato', 'Onions', 'Cucumber', 'Lemon', '']
+    num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    num_rm = rm.sample(num, k = num_num)
+    eat_rm = rm.sample(eat, k = eat_num)
+    print('Password: '+''.join(map(str, eat_rm + num_rm)))
---
+++ tests.py
@@ -0,0 +1,3 @@
+from generate_password52 import *
+pass_hard(12)
+pass_lite(1, 5)
